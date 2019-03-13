4999b330f84ccf8eab209774631c7babc21e4ae5
nickyshibaev@gmail.com <GrAnT0132>
2019-03-13 16:25:29 +0200

    insurance-broker

--- js/main.js
+++ js/main.js
@@ -60,20 +60,20 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */,
-/* 1 */,
-/* 2 */
+/******/ ({
+
+/***/ 4:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(3);
+module.exports = __webpack_require__(5);
 
 
 /***/ }),
-/* 3 */
+
+/***/ 5:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -289,5 +289,6 @@ $(window).on('scroll', function () {
 // });
 
 /***/ })
-/******/ ]);
+
+/******/ });
 //# sourceMappingURL=main.js.map
